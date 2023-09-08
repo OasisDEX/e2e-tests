9a6b9c5ae59117ead5cf8c9d0b03453409873469
juan-langa <juan@oazoapps.com>
2023-09-08 11:31:25 +0200

    Spark Borrow test

---
+++ tests/withWallet/aaveV2/aaveV2Earn.spec.ts
@@ -0,0 +1,67 @@
+import { BrowserContext, test } from '@playwright/test';
+import { expect, metamaskSetUp } from 'utils/setup';
+import { resetState } from '@synthetixio/synpress/commands/synpress';
+import * as metamask from '@synthetixio/synpress/commands/metamask';
+import * as tenderly from 'utils/tenderly';
+import { setup } from 'utils/setup';
+import { hooksTimeout, testTimeout } from 'utils/config';
+import { App } from 'src/app';
+
+let context: BrowserContext;
+let app: App;
+let forkId: string;
+let walletAddress: string;
+
+test.describe.configure({ mode: 'serial' });
+
+test.describe('Aave - Wallet connected', async () => {
+	test.beforeAll(async () => {
+		test.setTimeout(hooksTimeout);
+
+		({ context } = await metamaskSetUp());
+		let page = await context.newPage();
+		app = new App(page);
+
+		({ forkId, walletAddress } = await setup(app));
+	});
+
+	test.afterAll(async () => {
+		await tenderly.deleteFork(forkId);
+
+		await app.page.close();
+
+		await context.close();
+
+		await resetState();
+	});
+
+	test('It should open an Aave v2 Earn position', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: '11772',
+		});
+
+		test.setTimeout(testTimeout);
+
+		await app.page.goto('/ethereum/aave/v2/earn/stETHeth');
+		// Depositing collateral too quickly after loading page returns wrong simulation results
+		await app.position.setup.waitForComponentToBeStable();
+		await app.position.setup.deposit({ token: 'ETH', amount: '10.09' });
+		await app.position.setup.createSmartDeFiAccount();
+		// Confirmation button with same label
+		await app.position.setup.createSmartDeFiAccount();
+		await metamask.confirmAddToken();
+		await app.position.setup.continue();
+		await app.position.setup.openEarnPosition1Of2();
+
+		// Position creation randomly fails - Retyr until it's created.
+		await expect(async () => {
+			await app.position.setup.confirmOrRetry();
+			await metamask.confirmPermissionToSpend();
+			await app.position.setup.goToPositionShouldBeVisible();
+		}).toPass();
+
+		await app.position.setup.goToPosition();
+		await app.position.manage.shouldBeVisible('Manage Earn ');
+	});
+});
---
+++ tests/withWallet/spark/sparkBorrow.spec.ts
@@ -0,0 +1,108 @@
+import { BrowserContext, test } from '@playwright/test';
+import { expect, metamaskSetUp } from 'utils/setup';
+import { resetState } from '@synthetixio/synpress/commands/synpress';
+import * as metamask from '@synthetixio/synpress/commands/metamask';
+import * as tenderly from 'utils/tenderly';
+import { setup } from 'utils/setup';
+import { hooksTimeout, testTimeout } from 'utils/config';
+import { App } from 'src/app';
+
+let context: BrowserContext;
+let app: App;
+let forkId: string;
+let walletAddress: string;
+
+test.describe.configure({ mode: 'serial' });
+
+test.describe('Spark Borrow - Wallet connected', async () => {
+	test.beforeAll(async () => {
+		test.setTimeout(hooksTimeout);
+
+		({ context } = await metamaskSetUp());
+		let page = await context.newPage();
+		app = new App(page);
+
+		({ forkId, walletAddress } = await setup(app));
+	});
+
+	test.afterAll(async () => {
+		await tenderly.deleteFork(forkId);
+
+		await app.page.close();
+
+		await context.close();
+
+		await resetState();
+	});
+
+	test('It should open a Spark Borrow position', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: '11811',
+		});
+
+		test.setTimeout(testTimeout);
+
+		await app.page.goto('/ethereum/spark/v3/borrow/ethdai#simulate');
+		// Depositing collateral too quickly after loading page returns wrong simulation results
+		await app.position.overview.waitForComponentToBeStable();
+		await app.position.setup.deposit({ token: 'ETH', amount: '3' });
+		await app.position.setup.borrow({ token: 'DAI', amount: '100' });
+		await app.position.setup.createSmartDeFiAccount();
+		// Confirmation button with same label
+		await app.position.setup.createSmartDeFiAccount();
+		await metamask.confirmAddToken();
+		await app.position.setup.continue();
+		await app.position.setup.openBorrowPosition1Of2();
+
+		// Position creation randomly fails - Retyr until it's created.
+		await expect(async () => {
+			await app.position.setup.confirmOrRetry();
+			await metamask.confirmPermissionToSpend();
+			await app.position.setup.goToPositionShouldBeVisible();
+		}).toPass();
+
+		await app.position.setup.goToPosition();
+		await app.position.manage.shouldBeVisible('Manage ');
+	});
+
+	test.skip('It should list an opened Spark Borrow position in portfolio', async () => {
+		test.info().annotations.push(
+			{
+				type: 'Test case',
+				description: '11813',
+			},
+			{
+				type: 'Bug',
+				description: '10547',
+			}
+		);
+
+		test.setTimeout(testTimeout);
+
+		await app.page.goto(`/owner/${walletAddress}`);
+
+		await app.header.shouldHavePortfolioCount('1');
+		await app.portfolio.borrow.shouldHaveHeaderCount('1');
+		await app.portfolio.borrow.vaults.first.shouldHave({ assets: 'ETH/DAI' });
+	});
+
+	test.skip('It should open an Spark Borrow position from portfolio page', async () => {
+		test.info().annotations.push(
+			{
+				type: 'Test case',
+				description: '11812',
+			},
+			{
+				type: 'Bug',
+				description: '10547',
+			}
+		);
+
+		test.setTimeout(testTimeout);
+
+		await app.page.goto(`/owner/${walletAddress}`);
+		await app.portfolio.borrow.vaults.first.view();
+		await app.position.manage.shouldBeVisible('Manage collateral');
+	});
+});
