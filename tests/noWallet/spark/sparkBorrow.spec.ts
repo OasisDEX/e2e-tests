56a0c2aad5aae5e89e9c7d0c256df59b0e9f0f78
juan-langa <juan@oazoapps.com>
2023-11-02 17:11:38 +0100

    Spark- Simulate positions tests

---
+++ tests/noWallet/spark/sparkBorrow.spec.ts
@@ -0,0 +1,54 @@
+import { test } from '#noWalletFixtures';
+import { longTestTimeout } from 'utils/config';
+
+test.describe('Spark Borrow', async () => {
+	test('It should allow to simulate an Spark Borrow position before opening it - No wallet connected @regression', async ({
+		app,
+	}) => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: '12583',
+		});
+
+		test.setTimeout(longTestTimeout);
+
+		await app.page.goto('/ethereum/spark/v3/borrow/wstethdai#simulate');
+
+		// Depositing collateral too quickly after loading page returns wrong simulation results
+		await app.position.overview.waitForComponentToBeStable();
+		await app.position.setup.deposit({ token: 'WSTETH', amount: '19.12345' });
+
+		await app.position.overview.shouldHaveNetValueAfterPill('[0-9]{2},[0-9]{3}.[0-9]{2}');
+		await app.position.overview.shouldHaveExposureAfterPill({
+			amount: '19.12345',
+			token: 'WSTETH',
+		});
+		await app.position.setup.shouldHaveMaxBorrowingAmount({
+			token: 'DAI',
+			amount: '[1-6][0-9],[0-9]{3}.[0-9]{2}',
+		});
+		await app.position.setup.orderInformation.shouldHaveTotalCollateral({
+			token: 'WSTETH',
+			current: '0.00000',
+			future: '19.12345',
+		});
+
+		await app.position.setup.borrow({ token: 'DAI', amount: '20,000.1234' });
+
+		await app.position.overview.shouldHaveLiquidationPriceAfterPill('1,[0-9]{3}.[0-9]{2}');
+		await app.position.overview.shouldHaveLoanToValueAfterPill('[1-9][0-9].[0-9]{2}%');
+		await app.position.overview.shouldHaveBorrowCostAfterPill('[1-9].[0-9]{2}');
+		await app.position.overview.shouldHaveNetValueAfterPill('[1-3][0-9],[0-9]{3}.[0-9]{2}');
+		await app.position.overview.shouldHaveDebtAfterPill({ amount: '20,000.1234', token: 'DAI' });
+		await app.position.setup.orderInformation.shouldHaveOutstandingDebt({
+			token: 'DAI',
+			current: '0.00',
+			future: '20,000.1234',
+		});
+		await app.position.orderInformation.shouldHaveLTV({
+			current: '0.00',
+			future: '[0-9]{2}.[0-9]{2}',
+		});
+		await app.position.setup.orderInformation.shouldHaveTransactionFee({ fee: '0' });
+	});
+});
--- tests/noWallet/spark/sparkEarn.spec.ts
+++ tests/noWallet/spark/sparkEarn.spec.ts
@@ -1,7 +1,80 @@
 import { expect, test } from '#noWalletFixtures';
-import { veryLongTestTimeout } from 'utils/config';
+import { longTestTimeout, veryLongTestTimeout } from 'utils/config';
 
 test.describe('Spark Earn', async () => {
+	test('It should allow to simulate a Spark Earn position before opening it - No wallet connected @regression', async ({
+		app,
+	}) => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: '12581',
+		});
+
+		test.setTimeout(longTestTimeout);
+
+		await app.page.goto('/ethereum/spark/v3/earn/wstetheth#simulate');
+
+		// Depositing collateral too quickly after loading page returns wrong simulation results
+		await app.position.overview.waitForComponentToBeStable();
+		await app.position.setup.deposit({ token: 'WSTETH', amount: '21.12345' });
+
+		await app.position.overview.shouldHaveLiquidationPriceAfterPill('0.[0-9]{2}');
+		await app.position.overview.shouldHaveLoanToValueAfterPill('[1-5][0-9].[0-9]{2}%');
+		await app.position.overview.shouldHaveBorrowCostAfterPill('[0-5].[0-9]{2}%');
+		await app.position.overview.shouldHaveNetValueAfterPill('[0-9]{2}.[0-9]{2}');
+		await app.position.overview.shouldHaveExposureAfterPill({
+			amount: '[0-9]{2}.[0-9]{5}',
+			token: 'WSTETH',
+		});
+		await app.position.overview.shouldHaveDebtAfterPill({
+			amount: '[2-9].[0-9]{4}',
+			token: 'ETH',
+		});
+		await app.position.overview.shouldHaveMultipleAfterPill('1(.[0-9]{1,2})?');
+		await app.position.overview.shouldHaveBuyingPowerAfterPill({
+			amount: '[0-9]{2},[0-9]{3}(.[0-9]{1,2})?',
+		});
+		await app.position.setup.shouldHaveLiquidationPrice({
+			amount: '0.[0-9]{4} ETH',
+		});
+		await app.position.setup.shouldHaveLoanToValue('[1-4][0-9].[0-9]');
+		await app.position.setup.orderInformation.shouldHaveBuyingAmount({
+			tokenAmount: '[2-8].[0-9]{5}',
+			token: 'WSTETH',
+			dollarsAmount: '[0-9]{1,2},[0-9]{3}.[0-9]{2}',
+		});
+		await app.position.setup.orderInformation.shouldHaveFlashloanAmount({
+			token: 'ETH',
+			amount: '[0-9]{1,2}.[0-9]{5}',
+		});
+		await app.position.setup.orderInformation.shouldHavePriceImpact({
+			amount: '[0-2].[0-9]{4}',
+			percentage: '0.[0-9]{2}',
+		});
+		await app.position.setup.orderInformation.shouldHaveSlippageLimit('0.[0-9]{2}');
+		await app.position.setup.orderInformation.shouldHaveMultiply({
+			current: '1',
+			future: '[1-2](.[0-9]{1,2})?',
+		});
+		await app.position.setup.orderInformation.shouldHaveOutstandingDebt({
+			token: 'ETH',
+			current: '0.00000',
+			future: '[2-9].[0-9]{5}',
+		});
+		await app.position.setup.orderInformation.shouldHaveTotalCollateral({
+			token: 'WSTETH',
+			current: '0.00000',
+			future: '[0-9]{2}.[0-9]{5}',
+		});
+		await app.position.setup.orderInformation.shouldHaveLTV({
+			current: '0.00',
+			future: '[1-5][0-9].[0-9]{2}',
+		});
+		await app.position.setup.orderInformation.shouldHaveTransactionFee({
+			fee: '0',
+		});
+	});
+
 	test('It should open existent Spark Earn Ethereum vault page @regression', async ({ app }) => {
 		test.setTimeout(veryLongTestTimeout);
 
