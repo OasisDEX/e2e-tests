3504b594cea1b946aa35db1ef00af3df25ebaff8
juan-langa <juan@oazoapps.com>
2023-11-27 16:21:15 +0100

    Aave V3 Base - Earn - Adjust Up/Down and Close

--- tests/withWallet/aaveV3/base/aaveV3EarnBase.spec.ts
+++ tests/withWallet/aaveV3/base/aaveV3EarnBase.spec.ts
@@ -4,30 +4,17 @@ import { resetState } from '@synthetixio/synpress/commands/synpress';
 import * as metamask from '@synthetixio/synpress/commands/metamask';
 import * as tenderly from 'utils/tenderly';
 import { setup } from 'utils/setup';
-import { hooksTimeout, extremelyLongTestTimeout, baseUrl } from 'utils/config';
+import { extremelyLongTestTimeout, baseUrl, veryLongTestTimeout } from 'utils/config';
 import { App } from 'src/app';
 
 let context: BrowserContext;
 let app: App;
 let forkId: string;
+let walletAddress: string;
 
 test.describe.configure({ mode: 'serial' });
 
 test.describe('Aave V3 Earn - Base - Wallet connected', async () => {
-	test.skip(baseUrl.includes('staging') || baseUrl.includes('//summer.fi'));
-
-	test.beforeEach(async () => {
-		test.setTimeout(hooksTimeout);
-
-		({ context } = await metamaskSetUp({ network: 'base' }));
-		let page = await context.newPage();
-		app = new App(page);
-
-		({ forkId } = await setup({ app, network: 'base' }));
-
-		await tenderly.setCbEthBalance({ forkId, cbEthBalance: '50' });
-	});
-
 	test.afterAll(async () => {
 		await tenderly.deleteFork(forkId);
 
@@ -38,11 +25,127 @@ test.describe('Aave V3 Earn - Base - Wallet connected', async () => {
 		await resetState();
 	});
 
+	test('It should adjust risk of an existent Aave V3 Earn Base position - Down @regression', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: '13069',
+		});
+
+		test.setTimeout(extremelyLongTestTimeout);
+
+		await test.step('Test setup', async () => {
+			({ context } = await metamaskSetUp({ network: 'base' }));
+			let page = await context.newPage();
+			app = new App(page);
+
+			({ forkId, walletAddress } = await setup({ app, network: 'base' }));
+
+			await tenderly.setCbEthBalance({ forkId, cbEthBalance: '20' });
+		});
+
+		await tenderly.changeAccountOwner({
+			account: '0x773aa707436b0ea8829f1aae6dcfbe3ba8508a3e',
+			newOwner: walletAddress,
+			forkId,
+		});
+
+		await app.page.goto('/base/aave/v3/18#overview');
+
+		await app.position.manage.shouldBeVisible('Manage Earn position');
+
+		const initialLiqPrice = await app.position.manage.getLiquidationPrice();
+		const initialLoanToValue = await app.position.manage.getLoanToValue();
+
+		await app.position.manage.waitForSliderToBeEditable();
+		await app.position.manage.moveSlider({ value: 0.3 });
+
+		await app.position.manage.adjustRisk();
+		await app.position.manage.confirm();
+		await test.step('Metamask: ConfirmPermissionToSpend', async () => {
+			await metamask.confirmPermissionToSpend();
+		});
+		await app.position.manage.shouldShowSuccessScreen();
+		await app.position.manage.ok();
+
+		await app.position.manage.shouldBeVisible('Manage Earn position');
+		const updatedLiqPrice = await app.position.manage.getLiquidationPrice();
+		const updatedLoanToValue = await app.position.manage.getLoanToValue();
+		expect(updatedLiqPrice).toBeGreaterThan(initialLiqPrice);
+		expect(updatedLoanToValue).toBeLessThan(initialLoanToValue);
+	});
+
+	test('It should adjust risk of an existent Aave V3 Earn Base position - Up @regression', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: '13070',
+		});
+
+		test.setTimeout(veryLongTestTimeout);
+
+		await app.position.manage.shouldBeVisible('Manage Earn position');
+		const initialLiqPrice = await app.position.manage.getLiquidationPrice();
+		const initialLoanToValue = await app.position.manage.getLoanToValue();
+
+		await app.position.manage.waitForSliderToBeEditable();
+		await app.position.manage.moveSlider({ value: 0.7 });
+
+		await app.position.manage.adjustRisk();
+		await app.position.manage.confirm();
+		await test.step('Metamask: ConfirmPermissionToSpend', async () => {
+			await metamask.confirmPermissionToSpend();
+		});
+		await app.position.manage.shouldShowSuccessScreen();
+		await app.position.manage.ok();
+
+		await app.position.manage.shouldBeVisible('Manage Earn position');
+		const updatedLiqPrice = await app.position.manage.getLiquidationPrice();
+		const updatedLoanToValue = await app.position.manage.getLoanToValue();
+		expect(updatedLiqPrice).toBeLessThan(initialLiqPrice);
+		expect(updatedLoanToValue).toBeGreaterThan(initialLoanToValue);
+	});
+
+	test('It should close an existent Aave V3 Earn Base position - Close to debt token (ETH) @regression', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: '13071',
+		});
+
+		test.setTimeout(veryLongTestTimeout);
+
+		await app.position.manage.openManageOptions({ currentLabel: 'Adjust' });
+		await app.position.manage.select('Close position');
+
+		await app.position.manage.closeTo('ETH');
+		await app.position.manage.shouldHaveTokenAmountAfterClosing({
+			token: 'ETH',
+			amount: '[0-9]{1,2}.[0-9]{1,2}',
+		});
+		await app.position.manage.confirm();
+		await test.step('Metamask: ConfirmPermissionToSpend', async () => {
+			await metamask.confirmPermissionToSpend();
+		});
+
+		await app.position.manage.shouldShowSuccessScreen();
+		await app.position.manage.ok();
+
+		await app.page.goto('/base/aave/v3/18#overview');
+		await app.position.overview.shouldHaveLiquidationPrice({ price: '0.00', token: 'CBETH' });
+		await app.position.overview.shouldHaveLoanToValue('0.00');
+		await app.position.overview.shouldHaveBorrowCost('0.00');
+		await app.position.overview.shouldHaveNetValue({ value: '0.00', token: 'CBETH' });
+		await app.position.overview.shouldHaveExposure({ amount: '0.00000', token: 'CBETH' });
+		await app.position.overview.shouldHaveDebt({ amount: '0.0000', token: 'ETH' });
+		await app.position.overview.shouldHaveMultiple('1');
+		await app.position.overview.shouldHaveBuyingPower('0.00');
+	});
+
 	test('It should open an Aave V3 Earn Base position @regression', async () => {
 		test.info().annotations.push({
 			type: 'Test case',
 			description: '12471',
 		});
+
+		test.skip(baseUrl.includes('staging') || baseUrl.includes('//summer.fi'));
 
 		test.setTimeout(extremelyLongTestTimeout);
 
@@ -88,6 +191,4 @@ test.describe('Aave V3 Earn - Base - Wallet connected', async () => {
 		await app.position.setup.goToPosition();
 		await app.position.manage.shouldBeVisible('Manage ');
 	});
-
-	// Risk UP -> 13035
 });
