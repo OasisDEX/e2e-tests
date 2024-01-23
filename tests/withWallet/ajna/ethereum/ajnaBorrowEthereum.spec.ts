5ec8b7cbe32f261a40c5de1f303d5101053e0d12
juan-langa <juan@oazoapps.com>
2024-01-23 17:17:57 +0100

    Ajna Ethereum Borrow - More tests

--- tests/withWallet/ajna/ethereum/ajnaBorrowEthereum.spec.ts
+++ tests/withWallet/ajna/ethereum/ajnaBorrowEthereum.spec.ts
@@ -4,7 +4,12 @@ import { resetState } from '@synthetixio/synpress/commands/synpress';
 import * as metamask from '@synthetixio/synpress/commands/metamask';
 import * as tenderly from 'utils/tenderly';
 import { setup } from 'utils/setup';
-import { extremelyLongTestTimeout, longTestTimeout } from 'utils/config';
+import {
+	extremelyLongTestTimeout,
+	longTestTimeout,
+	positionTimeout,
+	veryLongTestTimeout,
+} from 'utils/config';
 import { App } from 'src/app';
 
 let context: BrowserContext;
@@ -45,7 +50,7 @@ test.describe('Ajna Ethereum Borrow - Wallet connected', async () => {
 				network: 'mainnet',
 				walletAddress,
 				token: 'WSTETH',
-				balance: '10',
+				balance: '100',
 			});
 		});
 
@@ -181,16 +186,212 @@ test.describe('Ajna Ethereum Borrow - Wallet connected', async () => {
 
 		await app.position.setup.continue();
 
-		// Position creation randomly fails - Retry until it's created.
-		await expect(async () => {
-			await app.position.setup.confirmOrRetry();
-			await test.step('Metamask: ConfirmPermissionToSpend', async () => {
-				await metamask.confirmPermissionToSpend();
-			});
-			await app.position.setup.goToPositionShouldBeVisible();
-		}).toPass({ timeout: longTestTimeout });
+		// ======================================================================
 
+		// UI sometimes gets stuck after confirming position creation
+		//   - 'Reload' added to avoid flakines
+
+		await app.position.setup.confirm();
+		await test.step('Metamask: ConfirmPermissionToSpend', async () => {
+			await metamask.confirmPermissionToSpend();
+		});
+		await app.position.setup.shouldShowCreatingPosition();
+
+		await app.page.reload();
 		await app.position.setup.goToPosition();
+
+		// ======================================================================
+
 		await app.position.manage.shouldBeVisible('Manage your Ajna Borrow Position');
+	});
+
+	test('It should Deposit and Borrow in a single tx form an existing Ajna Ethereum Borrow position @regression', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: 'xxxxx',
+		});
+
+		test.setTimeout(veryLongTestTimeout);
+
+		await app.position.setup.deposit({ token: 'WSTETH', amount: '15' });
+		await app.position.setup.borrow({ token: 'ETH', amount: '10' });
+
+		await app.position.setup.confirm();
+
+		// ============================================================
+
+		// UI sometimes gets stuck after confirming position update
+		//   - 'Reload' added to avoid flakines
+		await app.position.setup.confirm();
+		await test.step('Metamask: ConfirmPermissionToSpend', async () => {
+			await metamask.confirmPermissionToSpend();
+		});
+		await app.position.setup.shouldShowUpdatingPosition();
+		await app.page.reload();
+
+		// ============================================================
+
+		await app.position.overview.shouldHaveCollateralDeposited({ amount: '25.00', token: 'WSTETH' });
+		await app.position.overview.shouldHaveDebt({
+			amount: '17.[0-9]{2}([0-9]{1,2})?',
+			token: 'ETH',
+			protocol: 'Ajna',
+		});
+	});
+
+	test('It should Withdraw and Pay back in a single tx form an existing Ajna Ethereum Borrow position @regression', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: 'xxxxx',
+		});
+
+		test.setTimeout(veryLongTestTimeout);
+
+		await app.position.manage.withdrawCollateral();
+		await app.position.manage.withdraw({ token: 'WSTETH', amount: '10' });
+		await app.position.manage.payback({ token: 'ETH', amount: '5' });
+
+		await app.position.setup.confirm();
+
+		// ============================================================
+
+		// UI sometimes gets stuck after confirming position update
+		//   - 'Reload' added to avoid flakines
+		await app.position.setup.confirm();
+		await test.step('Metamask: ConfirmPermissionToSpend', async () => {
+			await metamask.confirmPermissionToSpend();
+		});
+		await app.position.setup.shouldShowUpdatingPosition();
+		await app.page.reload();
+
+		// ============================================================
+
+		await app.position.overview.shouldHaveCollateralDeposited({ amount: '15.00', token: 'WSTETH' });
+		await app.position.overview.shouldHaveDebt({
+			amount: '12.[0-9]{2}([0-9]{1,2})?',
+			token: 'ETH',
+			protocol: 'Ajna',
+		});
+	});
+
+	test('It should Borrow and Deposit in a single tx on an existing Ajna Ethereum Borrow position @regression', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: 'xxxx',
+		});
+
+		test.setTimeout(veryLongTestTimeout);
+
+		await app.position.manage.openManageOptions({ currentLabel: 'WSTETH' });
+		await app.position.manage.select('Manage debt');
+
+		await app.position.manage.borrow({ token: 'ETH', amount: '15' });
+		await app.position.manage.deposit({ token: 'WSTETH', amount: '20' });
+
+		await app.position.setup.confirm();
+
+		// ============================================================
+
+		// UI sometimes gets stuck after confirming position update
+		//   - 'Reload' added to avoid flakines
+		await app.position.setup.confirm();
+		await test.step('Metamask: ConfirmPermissionToSpend', async () => {
+			await metamask.confirmPermissionToSpend();
+		});
+		await app.position.setup.shouldShowUpdatingPosition();
+		await app.page.reload();
+
+		// ============================================================
+
+		await app.position.overview.shouldHaveCollateralDeposited({ amount: '35.00', token: 'WSTETH' });
+		await app.position.overview.shouldHaveDebt({
+			amount: '27.[0-9]{2}([0-9]{1,2})?',
+			token: 'ETH',
+			protocol: 'Ajna',
+		});
+	});
+
+	test('It should Pay back and Withdraw in a single tx on an existing Ajna Ethereum Borrow position @regression', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: 'xxxx',
+		});
+
+		test.setTimeout(veryLongTestTimeout);
+
+		await app.position.manage.openManageOptions({ currentLabel: 'WSTETH' });
+		await app.position.manage.select('Manage debt');
+
+		await app.position.manage.payBackDebt();
+		await app.position.manage.payback({ token: 'ETH', amount: '20' });
+		await app.position.manage.withdraw({ token: 'WSTETH', amount: '15' });
+
+		await app.position.setup.confirm();
+
+		// ============================================================
+
+		// UI sometimes gets stuck after confirming position update
+		//   - 'Reload' added to avoid flakines
+		await app.position.setup.confirm();
+		await test.step('Metamask: ConfirmPermissionToSpend', async () => {
+			await metamask.confirmPermissionToSpend();
+		});
+		await app.position.setup.shouldShowUpdatingPosition();
+		await app.page.reload();
+
+		// ============================================================
+
+		await app.position.overview.shouldHaveCollateralDeposited({ amount: '20.00', token: 'WSTETH' });
+		await app.position.overview.shouldHaveDebt({
+			amount: '7.[0-9]{2}([0-9]{1,2})?',
+			token: 'ETH',
+			protocol: 'Ajna',
+		});
+	});
+
+	test('It should Close to collateral an existing Ajna Ethereum Borrow position @regression', async () => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: 'xxxx',
+		});
+
+		test.setTimeout(veryLongTestTimeout);
+
+		await app.position.manage.openManageOptions({ currentLabel: 'WSTETH' });
+		await app.position.manage.select('Close position');
+		await app.position.manage.shouldHaveTokenAmountAfterClosing({
+			token: 'WSTETH',
+			amount: '[0-9]{1,2}.[0-9]{1,2}',
+		});
+
+		await app.position.setup.confirm();
+
+		// ============================================================
+
+		// UI sometimes gets stuck after confirming position update
+		//   - 'Reload' added to avoid flakines
+		await app.position.setup.confirm();
+		await test.step('Metamask: ConfirmPermissionToSpend', async () => {
+			await metamask.confirmPermissionToSpend();
+		});
+		await app.position.setup.shouldShowUpdatingPosition();
+		await app.page.reload();
+
+		// ============================================================
+
+		await app.position.overview.shouldHaveLiquidationPrice({
+			price: '0.00',
+			timeout: positionTimeout,
+		});
+		await app.position.overview.shouldHaveLoanToValue('0.00');
+		await app.position.overview.shouldHaveCollateralDeposited({ amount: '0.00', token: 'WSTETH' });
+		await app.position.overview.shouldHaveDebt({
+			amount: '0.00',
+			token: 'ETH',
+			protocol: 'Ajna',
+		});
+		await app.position.overview.shouldHaveNetValue({ value: '0.00' });
+		await app.position.overview.shouldHaveAvailableToWithdraw({ token: 'WSTETH', amount: '0.00' });
+		await app.position.overview.shouldHaveAvailableToBorrow({ token: 'ETH', amount: '0.00' });
 	});
 });
