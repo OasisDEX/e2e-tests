a8eaf8b283f08d94a098fb62ab88837aee5f38ee
juan-langa <juan@oazoapps.com>
2024-05-07 13:18:22 +0200

    Remove Refinance test from regression while investigating why it's failing in github

--- tests/withWallet/maker/refinance/wbtcDaiToBorrowSdaiEth.spec.ts
+++ tests/withWallet/maker/refinance/wbtcDaiToBorrowSdaiEth.spec.ts
@@ -31,7 +31,7 @@ test.describe('Maker Borrow - Wallet connected', async () => {
 	});
 
 	// Create a Maker position as part of the Refinance tests setup
-	test('It should open a Maker Borrow position @regression', async () => {
+	test('It should open a Maker Borrow position', async () => {
 		test.info().annotations.push({
 			type: 'Test case',
 			description: '11788, 11790',
@@ -101,7 +101,7 @@ test.describe('Maker Borrow - Wallet connected', async () => {
 		await app.position.manage.shouldHaveButton({ label: 'WBTC' });
 	});
 
-	test('It should refinance a Maker Borrow position (WBTC/DAI) to Spark Borrow (sDAI/ETH) @regression', async () => {
+	test('It should refinance a Maker Borrow position (WBTC/DAI) to Spark Borrow (sDAI/ETH)', async () => {
 		test.info().annotations.push({
 			type: 'Test case',
 			description: '11788, 11790',
