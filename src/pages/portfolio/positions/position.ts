4113329fe1e0af26a827ed225ce892d623c1142c
juan-langa <juan@oazoapps.com>
2024-05-09 17:10:39 +0200

    Tests for Stop-Loss ON - Portfolio and position page

--- src/pages/portfolio/positions/position.ts
+++ src/pages/portfolio/positions/position.ts
@@ -2,20 +2,23 @@ import { step } from '#noWalletFixtures';
 import { expect, Locator } from '@playwright/test';
 import { portfolioTimeout } from 'utils/config';
 
-type AutomationStatus = 'On' | 'Off';
+type AutomationStatus = {
+	automation: 'Stop-Loss' | 'Auto Sell' | 'Auto Buy' | 'Take Profit';
+	status: 'On' | 'Off';
+}[];
 
 export class Position {
-	readonly vaultLocator: Locator;
+	readonly positionLocator: Locator;
 
-	constructor(vaultLocator: Locator) {
-		this.vaultLocator = vaultLocator;
+	constructor(positionLocator: Locator) {
+		this.positionLocator = positionLocator;
 	}
 
 	@step
 	async shouldHave(args: { assets?: string }) {
 		if (args.assets) {
 			await expect(
-				this.vaultLocator.getByText('Position').locator('xpath=//preceding::span[1]')
+				this.positionLocator.getByText('Position').locator('xpath=//preceding::span[1]')
 			).toContainText(args.assets, { timeout: portfolioTimeout });
 		}
 	}
@@ -23,24 +26,31 @@ export class Position {
 	@step
 	async view() {
 		await expect(
-			this.vaultLocator.getByRole('button', { name: 'View' }),
+			this.positionLocator.getByRole('button', { name: 'View' }),
 			'"View" should be visible'
 		).toBeVisible({
 			timeout: portfolioTimeout,
 		});
-		await this.vaultLocator.getByRole('button', { name: 'View' }).click();
+		await this.positionLocator.getByRole('button', { name: 'View' }).click();
 	}
 
 	@step
-	async shouldHaveAutomations({
-		stopLoss,
-		autoSell,
-		autoBuy,
-		takeProfit,
-	}: {
-		stopLoss: AutomationStatus;
-		autoSell: AutomationStatus;
-		autoBuy: AutomationStatus;
-		takeProfit: AutomationStatus;
-	}) {}
+	async shouldHaveAutomations(automations: AutomationStatus) {
+		for (let i = 0; i < automations.length; i++) {
+			const automationIcon: Locator = await this.positionLocator
+				.getByText(automations[i].automation)
+				.locator('..');
+
+			const backgroundColor = await automationIcon.evaluate((el) => {
+				return window.getComputedStyle(el).getPropertyValue('background-color');
+			});
+
+			const actualStatus = backgroundColor === 'rgb(231, 252, 250)' ? 'On' : 'Off';
+
+			expect(
+				actualStatus,
+				`${automations[i].automation} should be ${automations[i].status}`
+			).toEqual(automations[i].status);
+		}
+	}
 }
--- tests/noWallet/portfolio/automations.spec.ts
+++ tests/noWallet/portfolio/automations.spec.ts
@@ -1,12 +1,16 @@
 import { test } from '#noWalletFixtures';
 
-test.describe.skip('Portfolio - Automations', async () => {
-	test('Portfolio - Active automation should be highlighted', async ({ app }) => {
+test.describe('Portfolio - Automations', async () => {
+	test('Portfolio - Should show current status of the position automations @regression', async ({
+		app,
+	}) => {
 		await app.portfolio.open('0x10649c79428d718621821Cf6299e91920284743F', { withPositions: true });
 
-		await app.portfolio.positionsHub.positions.byId('1143');
-		//
-		await app.pause();
-		//
+		await app.portfolio.positionsHub.positions.byId('1143').shouldHaveAutomations([
+			{ automation: 'Stop-Loss', status: 'On' },
+			{ automation: 'Auto Sell', status: 'Off' },
+			{ automation: 'Auto Buy', status: 'Off' },
+			{ automation: 'Take Profit', status: 'Off' },
+		]);
 	});
 });
