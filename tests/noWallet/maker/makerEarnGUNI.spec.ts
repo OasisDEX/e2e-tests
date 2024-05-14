2fac3fcef6c21ff60f0b0a77ddd616b52b3f4aff
juan-langa <juan@oazoapps.com>
2024-05-14 12:10:51 +0200

    Maker Earn GUNI test - Open position page

---
+++ tests/noWallet/maker/makerEarnGUNI.spec.ts
@@ -0,0 +1,30 @@
+import { test } from '#noWalletFixtures';
+
+test.describe.only('Maker Earn - GUNI', async () => {
+	test('It should open an existing Maker Earn GUNI vault page @regression', async ({ app }) => {
+		test.info().annotations.push({
+			type: 'Test case',
+			description: 'xxx',
+		});
+
+		await app.position.openPage('/ethereum/maker/29929#overview');
+
+		// Position name notdisplayed when wallet is not connected
+		// await app.position.shouldHaveHeader('GUNIV3DAIUSDC2-A');
+
+		await app.position.overview.shouldBeVisible();
+		await app.position.overview.shouldHaveNetValue({
+			value: '\\$([0-9]{1,2})?([0-9],)?([0-9]{1,2})?[0-9].[0-9]{2}',
+		});
+		await app.position.overview.shouldHaveTotalCollateral({
+			amount: '[0-9].[0-9]{2}',
+			token: 'GUNIV3DAIUSDC2',
+		});
+		await app.position.overview.shouldHaveDebt({
+			amount: '[0-9].[0-9]{4}',
+			token: 'DAI',
+		});
+
+		await app.position.manage.shouldBeVisible('Manage your vault');
+	});
+});
