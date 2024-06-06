cc5566f819a268cbd2a373b7da96afcd0a54d71c
juan-langa <juan@oazoapps.com>
2024-06-06 10:03:20 +0200

    Swap - Skipping tests

--- tests/withWallet/maker/swap/makerToSpark/multWbtcDaiToMultWstethDai.spec.ts
+++ tests/withWallet/maker/swap/makerToSpark/multWbtcDaiToMultWstethDai.spec.ts
@@ -14,7 +14,7 @@ let walletAddress: string;
 
 test.describe.configure({ mode: 'serial' });
 
-test.describe('Maker Multiply - Swap to Spark', async () => {
+test.describe.skip('Maker Multiply - Swap to Spark', async () => {
 	test.afterAll(async () => {
 		await tenderly.deleteFork(forkId);
 
