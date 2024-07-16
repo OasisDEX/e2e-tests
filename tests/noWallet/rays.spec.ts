353378e4a9e4937f4e52e34a4baf5703b6b9b9c6
juan-langa <juan@oazoapps.com>
2024-07-16 17:06:32 +0200

    Rays - No wllet - Part 2

--- tests/noWallet/rays.spec.ts
+++ tests/noWallet/rays.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '#noWalletFixtures';
+import { expect, test } from '#noWalletFixtures';
 import { longTestTimeout } from 'utils/config';
 
 test.describe('No-wallet connected - Rays', async () => {
@@ -28,5 +28,60 @@ test.describe('No-wallet connected - Rays', async () => {
 
 		await app.rays.connectWallet();
 		await app.modals.connectWallet.shouldBeVisible();
+	});
+
+	test('It should calculate rays to be earned with migration', async ({ app }) => {
+		await app.rays.openPage();
+		await app.rays.openCalculator();
+		await app.rays.calculator.shouldEstimateRays({
+			perYear: 0,
+			migration: 0,
+			afterOneYear: 0,
+		});
+
+		await app.rays.calculator.typeAmount('10000');
+		await app.rays.calculator.calculateRays();
+		await app.rays.calculator.shouldEstimateRays({
+			perYear: 670,
+			migration: 130,
+			afterOneYear: 800,
+		});
+	});
+
+	test('It should calculate rays to be earned without migration', async ({ app }) => {
+		await app.rays.openPage();
+		await app.rays.openCalculator();
+		await app.rays.calculator.shouldEstimateRays({
+			perYear: 0,
+			migration: 0,
+			afterOneYear: 0,
+		});
+
+		await app.rays.calculator.typeAmount('20000');
+		await app.rays.calculator.selectMigration('No');
+		await app.rays.calculator.calculateRays();
+		await app.rays.calculator.shouldEstimateRays({
+			perYear: 1400,
+			migration: 0,
+			afterOneYear: 1400,
+		});
+	});
+
+	test('It should have 5x Leaderboard results by default', async ({ app }) => {
+		await app.rays.openPage();
+		await app.rays.leaderboard.shouldHaveResults(5);
+	});
+
+	test('It should redirect to Leaderboard page', async ({ app }) => {
+		await app.rays.openPage();
+		await app.rays.leaderboard.shouldNotHaveNextPage();
+
+		await app.rays.leaderboard.viewFullLeaderboard();
+		await app.rays.leaderboard.shouldHaveNextPage();
+	});
+
+	test('It should show 100x Leaderboard results per page', async ({ app }) => {
+		await app.rays.leaderboard.openPage();
+		await app.rays.leaderboard.shouldHaveResults(100);
 	});
 });
