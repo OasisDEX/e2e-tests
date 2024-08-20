1bfbd3f02b1d3c683ec483cf844dbc03e986ad79
juan-langa <juan@oazoapps.com>
2024-08-20 11:35:47 +0200

    Daily improvements - 20 August

--- tests/withWallet/z_openPositions/aaveV3/base/aavev3Earn.spec.ts
+++ tests/withWallet/z_openPositions/aaveV3/base/aavev3Earn.spec.ts
@@ -3,7 +3,8 @@ import { aaveV3BaseEarnPools } from 'utils/testData';
 import { openNewPosition } from 'tests/sharedTestSteps/openNewPosition';
 
 aaveV3BaseEarnPools.forEach((pool) => {
-	test.describe('Aave V3 Earn - Wallet connected', async () => {
+	// Base ETH borrow cap at 100%
+	test.describe.skip('Aave V3 Earn - Wallet connected', async () => {
 		await openNewPosition({
 			network: 'base',
 			protocol: 'aave/v3',
