993a1d09513f5d624ea1f28f4017c835aeabb0e4
juan-langa <juan@oazoapps.com>
2024-08-29 19:03:09 +0200

    API tests - Part 3

---
+++ utils/testData_APIs.ts
@@ -0,0 +1,233 @@
+import { expect } from '#noWalletFixtures';
+
+export const responses = {
+	autoBuyWithoutMaxBuyPrice: {
+		simulation: {
+			executionLTV: '3200',
+			targetLTV: '5000',
+			collateralAmountAfterExecution: expect.any(String),
+			debtAmountAfterExecution: expect.any(String),
+			targetLTVWithDeviation: ['4900', '5100'],
+			targetMultiple: expect.any(String),
+			executionPrice: expect.any(String),
+			position: {
+				hasStablecoinDebt: true,
+				ltv: expect.any(String),
+				collateral: {
+					balance: expect.any(String),
+					token: {
+						decimals: 18,
+						symbol: 'WETH',
+						address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+					},
+				},
+				debt: {
+					balance: expect.any(String),
+					token: {
+						decimals: 6,
+						symbol: 'USDC',
+						address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+					},
+				},
+				address: '0x16F2C35E062C14F57475dE0A466F7E08b03A9C7D',
+				oraclePrices: {
+					collateralPrice: expect.any(String),
+					debtPrice: expect.any(String),
+				},
+				collateralPriceInDebt: expect.any(String),
+				netValueUSD: expect.any(String),
+				debtValueUSD: expect.any(String),
+				collateralValueUSD: expect.any(String),
+			},
+		},
+		transaction: {
+			to: '0x16F2C35E062C14F57475dE0A466F7E08b03A9C7D',
+			data: expect.any(String),
+			triggerTxData: expect.any(String),
+		},
+		encodedTriggerData: expect.any(String),
+		warnings: [
+			{
+				message: 'Auto buy with no max price threshold',
+				code: 'auto-buy-with-no-max-price-threshold',
+				path: [],
+			},
+		],
+	},
+	wrongDpm: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Invalid address format',
+				path: ['dpm'],
+			},
+		],
+		warnings: [],
+	},
+	missingPosition: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Required',
+				code: 'invalid_type',
+				path: ['position'],
+			},
+		],
+		warnings: [],
+	},
+	wrongPosition_string: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Expected object, received string',
+				code: 'invalid_type',
+				path: ['position'],
+			},
+		],
+		warnings: [],
+	},
+	wrongPosition_number: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Expected object, received number',
+				code: 'invalid_type',
+				path: ['position'],
+			},
+		],
+		warnings: [],
+	},
+	wrongPosition_array: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Expected object, received array',
+				code: 'invalid_type',
+				path: ['position'],
+			},
+		],
+		warnings: [],
+	},
+	wrongPosition_null: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Expected object, received null',
+				code: 'invalid_type',
+				path: ['position'],
+			},
+		],
+		warnings: [],
+	},
+	wrongCollateral: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Invalid address format',
+				path: ['position', 'collateral'],
+			},
+		],
+		warnings: [],
+	},
+	wrongDebt: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Invalid address format',
+				path: ['position', 'debt'],
+			},
+		],
+		warnings: [],
+	},
+	missingTriggerData: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Required',
+				code: 'invalid_type',
+				path: ['triggerData'],
+			},
+		],
+		warnings: [],
+	},
+	wrongTriggerData_string: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Expected object, received string',
+				code: 'invalid_type',
+				path: ['triggerData'],
+			},
+		],
+		warnings: [],
+	},
+	wrongTriggerData_number: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Expected object, received number',
+				code: 'invalid_type',
+				path: ['triggerData'],
+			},
+		],
+		warnings: [],
+	},
+	wrongTriggerData_array: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Expected object, received array',
+				code: 'invalid_type',
+				path: ['triggerData'],
+			},
+		],
+		warnings: [],
+	},
+	wrongTriggerData_null: {
+		message: 'Validation Errors',
+		errors: [
+			{
+				message: 'Expected object, received null',
+				code: 'invalid_type',
+				path: ['triggerData'],
+			},
+		],
+		warnings: [],
+	},
+};
+
+export const validPayloads = {
+	autoBuy: {
+		addWithoutMaxBuyPrice: {
+			action: 'add',
+			dpm: '0x16f2c35e062c14f57475de0a466f7e08b03a9c7d',
+			position: {
+				collateral: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+				debt: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+			},
+			protocol: 'aavev3',
+			triggerData: {
+				executionLTV: '3200',
+				maxBaseFee: '300',
+				targetLTV: '5000',
+				useMaxBuyPrice: false,
+			},
+		},
+		addWithMaxBuyPrice: {
+			action: 'add',
+			dpm: '0x16f2c35e062c14f57475de0a466f7e08b03a9c7d',
+			position: {
+				collateral: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+				debt: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+			},
+			protocol: 'aavev3',
+			triggerData: {
+				executionLTV: '3200',
+				maxBaseFee: '300',
+				maxBuyPrice: '1000000000000',
+				targetLTV: '5000',
+				useMaxBuyPrice: true,
+			},
+		},
+	},
+};
