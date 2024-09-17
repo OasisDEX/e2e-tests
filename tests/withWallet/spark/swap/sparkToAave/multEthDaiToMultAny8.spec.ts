8af823e0d9585b6ca86d5270308fa90273d07a5b
juan-langa <juan@oazoapps.com>
2024-09-17 12:22:30 +0200

    Daily improvements - 17 September

--- tests/withWallet/spark/swap/sparkToAave/multEthDaiToMultAny8.spec.ts
+++ tests/withWallet/spark/swap/sparkToAave/multEthDaiToMultAny8.spec.ts
@@ -3,9 +3,9 @@ import { resetState } from '@synthetixio/synpress/commands/synpress';
 import { metamaskSetUp } from 'utils/setup';
 import * as tenderly from 'utils/tenderly';
 import { setup } from 'utils/setup';
-import { extremelyLongTestTimeout, longTestTimeout } from 'utils/config';
+import { extremelyLongTestTimeout } from 'utils/config';
 import { App } from 'src/app';
-import { openMakerPosition, swapPosition } from 'tests/sharedTestSteps/positionManagement';
+import { openPosition, swapPosition } from 'tests/sharedTestSteps/positionManagement';
 
 let context: BrowserContext;
 let app: App;
@@ -13,7 +13,7 @@ let forkId: string;
 
 test.describe.configure({ mode: 'serial' });
 
-test.describe('Maker Borrow - Swap to Aave V3', async () => {
+test.describe('Spark Multiply - Swap to Aave V3', async () => {
 	test.afterAll(async () => {
 		await tenderly.deleteFork(forkId);
 
@@ -28,11 +28,11 @@ test.describe('Maker Borrow - Swap to Aave V3', async () => {
 		viewport: { width: 1400, height: 720 },
 	});
 
-	// Create a Maker position as part of the Swap tests setup
-	test('Test setup - Open Maker Borrow position and start Swap process', async () => {
+	// Create a Spark position as part of the Swap tests setup
+	test('It should open a Spark Multiply position', async () => {
 		test.info().annotations.push({
 			type: 'Test case',
-			description: '11788, 11790',
+			description: 'xxx',
 		});
 
 		test.setTimeout(extremelyLongTestTimeout);
@@ -49,16 +49,15 @@ test.describe('Maker Borrow - Swap to Aave V3', async () => {
 			}));
 		});
 
-		await app.page.goto('vaults/open/ETH-C');
+		await app.page.goto('/ethereum/spark/multiply/ETH-DAI#setup');
 
 		// Depositing collateral too quickly after loading page returns wrong simulation results
-		await app.position.overview.waitForComponentToBeStable({ positionType: 'Maker' });
+		await app.position.overview.waitForComponentToBeStable();
 
-		await openMakerPosition({
+		await openPosition({
 			app,
 			forkId,
 			deposit: { token: 'ETH', amount: '10' },
-			generate: { token: 'DAI', amount: '5000' },
 		});
 
 		await app.page.waitForTimeout(3000);
@@ -66,30 +65,27 @@ test.describe('Maker Borrow - Swap to Aave V3', async () => {
 		await swapPosition({
 			app,
 			forkId,
-			reason: 'Change direction of my position',
-			originalProtocol: 'Maker',
-			targetProtocol: 'Aave V3',
-			targetPool: { colToken: 'SDAI', debtToken: 'ETH' },
+			reason: 'Switch to higher max Loan To Value',
+			targetPool: { colToken: 'ETH', debtToken: 'DAI' },
 			upToStep5: true,
 		});
 	});
 
 	(
 		[
-			{ colToken: 'SDAI', debtToken: 'ETH' },
-			// { colToken: 'SDAI', debtToken: 'GHO' }, - NO LIQUIDITY - GHO
-			{ colToken: 'SDAI', debtToken: 'USDT' },
-			{ colToken: 'SDAI', debtToken: 'WBTC' },
-			{ colToken: 'USDC', debtToken: 'ETH' },
+			{ colToken: 'ETH', debtToken: 'USDC' },
+			{ colToken: 'ETH', debtToken: 'USDT' },
+			{ colToken: 'ETH', debtToken: 'WBTC' },
+			{ colToken: 'LDO', debtToken: 'USDT' },
 		] as const
 	).forEach((targetPool) =>
-		test(`It should swap a Maker Borrow position (ETH/DAI) to Aave V3 Multiply (${targetPool.colToken}/${targetPool.debtToken})`, async () => {
+		test(`It should swap a Spark Multiply position (ETH/DAI) to Aave V3 Multiply (${targetPool.colToken}/${targetPool.debtToken})`, async () => {
 			test.info().annotations.push({
 				type: 'Test case',
 				description: 'xxx',
 			});
 
-			test.setTimeout(longTestTimeout);
+			test.setTimeout(extremelyLongTestTimeout);
 
 			// Wait an reload to avoid flakiness
 			await app.page.waitForTimeout(1000);
@@ -99,8 +95,6 @@ test.describe('Maker Borrow - Swap to Aave V3', async () => {
 				app,
 				forkId,
 				reason: 'Switch to higher max Loan To Value',
-				originalProtocol: 'Maker',
-				targetProtocol: 'Aave V3',
 				targetPool: { colToken: targetPool.colToken, debtToken: targetPool.debtToken },
 				existingDpmAndApproval: true,
 				rejectSwap: true,
