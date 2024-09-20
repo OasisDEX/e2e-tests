3ed1b5d50e36ed3c375af929bba9697f9cd28e5e
juan-langa <juan@oazoapps.com>
2024-09-20 11:26:04 +0200

    Daily improvements - 20 September - Create fork with only ETH banace - Add balance for other tokens

---
+++ utilsManualTesting/addBalanceToWallet.ts
@@ -0,0 +1,32 @@
+import { SetBalanceTokens } from 'utils/testData';
+import * as tenderly from '../utils/tenderly';
+
+type Networks = 'mainnet' | 'optimism' | 'arbitrum' | 'base';
+
+const { NETWORK, WALLET_ADDRESS, FORK_ID, TOKENS } = process.env;
+const network = NETWORK as Networks;
+const forkId = FORK_ID;
+const tokens = TOKENS.split(' ') as SetBalanceTokens[];
+
+let walletAddress: string = WALLET_ADDRESS;
+
+(async () => {
+	for (const token of tokens) {
+		await tenderly.setTokenBalance({
+			forkId,
+			network,
+			token,
+			balance: tenderly.tokenBalances[token],
+			walletAddress: walletAddress,
+		});
+	}
+
+	console.log(`Selected network: ${NETWORK}`);
+	console.log(`Wallet address: ${WALLET_ADDRESS}`);
+	console.log('-------------------------------------');
+	tokens.forEach((token) => {
+		console.log(`${token} - ${tenderly.tokenBalances[token]}`);
+	});
+	console.log('-------------------------------------');
+	console.log('Fork RPC: ', `https://rpc.tenderly.co/fork/${forkId}`);
+})();
