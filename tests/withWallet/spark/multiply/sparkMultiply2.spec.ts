4ddecb9577d0c61e5d94b9da227410cbe0f45b96
juan-langa <juan@oazoapps.com>
2024-09-23 17:30:39 +0200

    Daily improvements - 23 September

--- tests/withWallet/spark/multiply/sparkMultiply2.spec.ts
+++ tests/withWallet/spark/multiply/sparkMultiply2.spec.ts
@@ -29,7 +29,7 @@ test.describe('Spark Multiply - Wallet connected', async () => {
 		await resetState();
 	});
 
-	test('It should open a Spark Multiply Short position @regression', async () => {
+	test('It should open a Spark Multiply Short position - SDAI/ETH @regression', async () => {
 		test.info().annotations.push({
 			type: 'Test case',
 			description: '12463',
