8d7523081baab7dd61263f648348d1f87be003f8
juan-langa <juan@oazoapps.com>
2024-10-18 15:55:21 +0200

    Daily improvements - 18 October

--- tests/noWallet/aaveV3/ethereum/aaveV3MultiplyEthereum.spec.ts
+++ tests/noWallet/aaveV3/ethereum/aaveV3MultiplyEthereum.spec.ts
@@ -65,45 +65,6 @@ test.describe('Aave v3 Multiply Ethereum', async () => {
 		);
 	});
 
-	test('It should open an existing Aave V3 Multiply Ethereum vault page @regression', async ({
-		app,
-	}) => {
-		test.setTimeout(longTestTimeout);
-
-		test.info().annotations.push({
-			type: 'Test case',
-			description: '11995',
-		});
-
-		await app.page.goto('/ethereum/aave/v3/multiply/ETH-USDC/1218#overview');
-
-		await app.position.shouldHaveHeader('ETH/USDC Multiply #1218');
-		await app.position.overview.shouldHaveLiquidationPrice({
-			price: '([1-3],)?[0-9]{3}.[0-9]{2}',
-			token: 'ETH/USDC',
-		});
-		await app.position.overview.shouldHaveLoanToValue('[1-8][0-9].[0-9]{2}');
-		await app.position.overview.shouldHaveNetValue({ value: '\\$[0-9]{1,2}.[0-9]{2}' });
-		await app.position.overview.shouldHaveBuyingPower('\\$[0-9]{2,3}.[0-9]{2}');
-		await app.position.overview.shouldHaveBuyingPowerGreaterThanZero();
-		await app.position.overview.shouldHaveExposure({
-			amount: '0.[0-9]{4}',
-			token: 'ETH',
-		});
-		await app.position.overview.shouldHaveExposureGreaterThanZero('ETH');
-		await app.position.overview.shouldHaveDebt({
-			amount: '[0-9]{1,2}.[0-9]{2}',
-			token: 'USDC',
-		});
-		await app.position.overview.shouldHaveBorrowRate('[0-9].[0-9]{2}');
-
-		await app.position.setup.shouldHaveLiquidationPrice({
-			amount: '([1-3],)?[0-9]{2,3}.[0-9]{2}',
-			pair: 'ETH/USDC',
-		});
-		await app.position.setup.shouldHaveLoanToValue('[0-9]{1,2}.[0-9]{1,2}');
-	});
-
 	test('It should show if position automation is ON - Stop-Loss @regression', async ({ app }) => {
 		await app.position.openPage('/ethereum/aave/v3/multiply/RETH-DAI/1276#overview');
 
---
+++ tests/noWallet/openExistingPositionPages/AaveV3/base.spec.ts
@@ -0,0 +1,47 @@
+import { test } from '#noWalletFixtures';
+import { longTestTimeout } from 'utils/config';
+
+test.describe('Open exisiting position pages - Aave v3 Base', async () => {
+	test('It should open an existing Aave V3 Base Earn yield loop CBETH/ETH position page @regression', async ({
+		app,
+	}) => {
+		test.setTimeout(longTestTimeout);
+
+		await app.page.goto('/base/aave/v3/earn/CBETH-ETH/376#overview');
+
+		await app.position.shouldHaveHeader('CBETH/ETH yield multiple #376');
+		await app.position.overview.shouldHaveNetValue({
+			value: '[0-9].[0-9]{2}',
+		});
+	});
+
+	test('It should open an existing Aave V3 Base Borrow ETH/USDBC position page @regression', async ({
+		app,
+	}) => {
+		test.setTimeout(longTestTimeout);
+
+		await app.page.goto('/base/aave/v3/borrow/ETH-USDBC/441#overview');
+
+		await app.position.shouldHaveHeader('ETH/USDBC Borrow #441');
+		await app.position.overview.shouldHaveLiquidationPrice({
+			price: '0.00',
+			token: 'ETH/USDBC',
+		});
+		await app.position.overview.shouldHaveLoanToValue('0.00');
+	});
+
+	test('It should open an existing Aave V3 Base Multiply ETH/USDC position page @regression', async ({
+		app,
+	}) => {
+		test.setTimeout(longTestTimeout);
+
+		await app.page.goto('/base/aave/v3/multiply/ETH-USDC/435#overview');
+
+		await app.position.shouldHaveHeader('ETH/USDC Multiply #435');
+		await app.position.overview.shouldHaveLiquidationPrice({
+			price: '([0-9],)[0-9]{2,3}.[0-9]{2}',
+			token: 'ETH/USDC',
+		});
+		await app.position.overview.shouldHaveLoanToValue('[0-9]{2}.[0-9]{2}');
+	});
+});
