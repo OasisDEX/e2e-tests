1fe1c2bc7e1f9cab5882f0e4d2379c3bd81468c6
juan-langa <juan@oazoapps.com>
2024-11-20 17:12:48 +0100

    Daily improvements - 20 November

---
+++ tests/withWallet/aaveV3/ethereum/swap/aaveToSpark.spec.ts
@@ -0,0 +1,24 @@
+import {
+	openNewPositionAndSwap,
+	Scenario,
+	test,
+} from 'tests/sharedTestSteps/openNewPositionAndSwap';
+
+(
+	[
+		{
+			pool: 'ETH-DAI',
+			positionType: 'borrow',
+			targetPools: [{ colToken: 'WEETH', debtToken: 'DAI' }],
+		},
+		{
+			pool: 'WSTETH-USDC',
+			positionType: 'multiply',
+			targetPools: [{ colToken: 'WSTETH', debtToken: 'DAI' }],
+		},
+	] as Scenario[]
+).forEach((scenario) =>
+	test.describe('Swap from Aave V3 to Spark', async () => {
+		await openNewPositionAndSwap({ ...scenario, protocol: 'aave/v3', targetProtocol: 'Spark' });
+	})
+);
