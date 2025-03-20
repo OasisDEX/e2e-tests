9b0d45bedcb635f4eb028e0220e461c5e76e0e70
juan-langa <juan@oazoapps.com>
2025-03-20 17:54:14 +0100

    Daily improvements - March - 20

--- tests/noWallet/openExistingPositionPages/AaveV3/optimism.spec.ts
+++ tests/noWallet/openExistingPositionPages/AaveV3/optimism.spec.ts
@@ -29,21 +29,21 @@ test.describe('Open exisiting position pages - Aave v3 Optimism', async () => {
 
 		await app.position.shouldHaveHeader('DAI/WBTC Borrow #4');
 		await app.position.overview.shouldHaveLiquidationPrice({
-			price: '<0.001',
+			price: '0.00',
 			token: 'WBTC/DAI',
 			timeout: positionTimeout,
 		});
-		await app.position.overview.shouldHaveLoanToValue('>110.00');
+		await app.position.overview.shouldHaveLoanToValue('0.00');
 		await app.position.overview.shouldHaveCollateralDeposited({
 			amount: '<0.001',
 			token: 'DAI',
 		});
 		await app.position.overview.shouldHaveDebt({
-			amount: '<0.001',
+			amount: '0.00',
 			token: 'WBTC',
 		});
 		await app.position.overview.shouldHaveBorrowRate('[0-9]{1,2}.[0-9]{2}');
-		await app.position.overview.shouldHaveNetValue({ value: '-0.[0-9]{1,2}' });
+		await app.position.overview.shouldHaveNetValue({ value: '<0.01' });
 	});
 
 	test('It should open an existing Aave V3 Multiply Optimism WBTC/USDC.E position page @regression', async ({
