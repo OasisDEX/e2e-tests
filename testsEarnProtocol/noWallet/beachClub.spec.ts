062cf697ea2105c1f10cb5544fbf2a15f08043bb
juan-langa <juan@oazoapps.com>
2025-06-09 15:47:46 -0400

    Daily improvements - June - 09

--- testsEarnProtocol/noWallet/beachClub.spec.ts
+++ testsEarnProtocol/noWallet/beachClub.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '#earnProtocolFixtures';
+import { expect, test } from '#earnProtocolFixtures';
 import { expectDefaultTimeout } from 'utils/config';
 
 test.describe('Beach Club Landing page', async () => {
@@ -13,8 +13,28 @@ test.describe('Beach Club Landing page', async () => {
 	});
 });
 
-// test.describe('Beach Club - Portfolio page', async () => {
-// 	test('It should not allow to generate a code', async ({app}) => {
-// 		await app.
-// 	});
-// });
+test.describe('Beach Club - Portfolio page', async () => {
+	test('It should copy referral code and referral link', async ({ app, context }) => {
+		await app.portfolio.beachClub.openPage('0x10649c79428d718621821cf6299e91920284743f');
+		await app.portfolio.beachClub.copyReferralCode();
+
+		await context.grantPermissions(['clipboard-read']);
+		const clipboardText1 = await app.page.evaluate(() => navigator.clipboard.readText());
+		expect(clipboardText1, 'Should have copied referral code in the clipboard').toBe('beach-test');
+	});
+
+	test('It should copy referral link', async ({ app, context }) => {
+		await app.portfolio.beachClub.openPage('0x10649c79428d718621821cf6299e91920284743f');
+		await app.portfolio.beachClub.copyReferralLink();
+
+		await context.grantPermissions(['clipboard-read']);
+		const clipboardText2 = await app.page.evaluate(() => navigator.clipboard.readText());
+		expect(clipboardText2, 'Should have copied referral link in the clipboard').toContain(
+			'/earn?referralCode=beach-test'
+		);
+	});
+
+	// 	test('It should not allow to generate a code', async ({app}) => {
+	// 		await app.
+	// 	});
+});
