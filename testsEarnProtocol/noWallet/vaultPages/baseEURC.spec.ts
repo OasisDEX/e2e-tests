7a0ee08cf1f05f3046ceef18c4b460c735be4393
juan-langa <juan@oazoapps.com>
2025-06-13 15:56:28 -0400

    Daily improvements - June - 13

--- testsEarnProtocol/noWallet/vaultPages/baseEURC.spec.ts
+++ testsEarnProtocol/noWallet/vaultPages/baseEURC.spec.ts
@@ -1,37 +1,30 @@
 import { expect, test } from '#earnProtocolFixtures';
 import { expectDefaultTimeout } from 'utils/config';
 
-test.describe('Vault page - Base - Specific user page', async () => {
+test.describe('Vault page - Base EURC', async () => {
 	test.beforeEach(async ({ app }, testInfo) => {
 		testInfo.setTimeout(testInfo.timeout + 30_000);
 
-		await app.vaultPage.open('/earn/base/position/0x98c49e13bf99d7cad8069faa2a370933ec9ecf17');
+		await app.vaultPage.open('/earn/base/position/0x64db8f51f1bf7064bb5a361a7265f602d348e0f0');
 	});
 
 	test('It should show 30d APY, Live APY, Assets in vault and Deposit Cap info', async ({
 		app,
 	}) => {
-		await app.vaultPage.shouldHave30dApy('[0-9].[0-9]{2}');
+		await app.vaultPage.shouldHave30dApy('[0-9]{1,2}.[0-9]{2}');
 
-		await app.vaultPage.shouldHaveLiveApy('[0-9].[0-9]{2}');
+		await app.vaultPage.shouldHaveLiveApy('[0-9]{1,2}.[0-9]{2}');
 
 		await app.vaultPage.shouldHaveAssets({
-			token: 'USDC',
+			token: 'EURC',
 			tokenAmount: '[0-9]{1,2}.[0-9]{2}M',
 			usdAmount: '[0-9]{1,2}.[0-9]{2}M',
 		});
 
 		await app.vaultPage.shouldHaveDepositCap({
-			token: 'USDC',
-			tokenAmount: '[0-9]{2,3}.[0-9]{2}M',
+			token: 'EURC',
+			tokenAmount: '[0-9]{1,2}.[0-9]{2}M',
 		});
-	});
-
-	test('It should show strategies exposure and be 100% in total', async ({ app }) => {
-		const totalAllocation = await app.vaultPage.exposure.getStrategiesTotalAllocation();
-
-		expect(totalAllocation).toBeGreaterThan(99);
-		expect(totalAllocation).toBeLessThanOrEqual(100);
 	});
 
 	test('It should show "How it works" page', async ({ app }) => {
@@ -51,5 +44,16 @@ test.describe('Vault page - Base - Specific user page', async () => {
 		await app.vaultPage.howItWorks.shouldHaveHeader('Lower Risk Historical Yields');
 
 		await app.vaultPage.howItWorks.shouldHaveHeader('Security');
+	});
+
+	test('It should show strategies exposure and be 100% in total', async ({ app }) => {
+		const totalAllocation = await app.vaultPage.exposure.getStrategiesTotalAllocation();
+
+		expect(totalAllocation).toBeGreaterThan(99);
+		expect(totalAllocation).toBeLessThanOrEqual(100);
+	});
+
+	test('It should not have duplicated strategy names', async ({ app }) => {
+		await app.vaultPage.exposure.shouldNotHaveDuplicatedStrategyNames();
 	});
 });
