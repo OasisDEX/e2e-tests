2da9fd44a91efd529796de1072a7e870be24d76f
juan-langa <juan@oazoapps.com>
2025-06-16 11:55:15 -0400

    Daily improvements - June - 16

--- testsEarnProtocol/noWallet/vaultPages/mainnetUSDC_LowerRisk.spec.ts
+++ testsEarnProtocol/noWallet/vaultPages/mainnetUSDC_LowerRisk.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '#earnProtocolFixtures';
+import { expectDefaultTimeout } from 'utils/config';
 
 test.describe('Vault page - Mainnet USDC Lower Risk', async () => {
 	test.beforeEach(async ({ app }, testInfo) => {
@@ -24,6 +25,25 @@ test.describe('Vault page - Mainnet USDC Lower Risk', async () => {
 			token: 'USDC',
 			tokenAmount: '[0-9]{2,3}.[0-9]{2}M',
 		});
+	});
+
+	test('It should show "How it works" page', async ({ app }) => {
+		await app.vaultPage.howItAllWorks();
+
+		await app.vaultPage.howItWorks.shouldHaveHeader('How it all works', {
+			timeout: expectDefaultTimeout * 2,
+		});
+		await app.vaultPage.howItWorks.shouldLinkToLitePaper();
+		await app.vaultPage.howItWorks.shouldHaveTabActive('Rebalance mechanism');
+		await app.vaultPage.howItWorks.shouldHaveImage('how-it-works');
+
+		await app.vaultPage.howItWorks.selectTab('Governance');
+		await app.vaultPage.howItWorks.shouldHaveTabActive('Governance');
+		await app.vaultPage.howItWorks.shouldHaveImage('governance');
+
+		await app.vaultPage.howItWorks.shouldHaveHeader('Lower Risk Historical Yields');
+
+		await app.vaultPage.howItWorks.shouldHaveHeader('Security');
 	});
 
 	test('It should show strategies exposure and be 100% in total', async ({ app }) => {
