3e30f0d9b29467ed359833db14947847253786f5
juan-langa <juan@oazoapps.com>
2025-10-28 14:28:29 +0100

    Institutions - October - 28

--- testsEarnProtocol/withRealWallet/mainnet/usdtPositionPage.spec.ts
+++ testsEarnProtocol/withRealWallet/mainnet/usdtPositionPage.spec.ts
@@ -113,10 +113,10 @@ test.describe('With real wallet - Mainnet USDT Position page - Deposit', async (
 			depositedToken: 'WETH',
 			depositAmount: '0.001',
 			estimatedEarnings: {
-				thirtyDaysAmount: '[1-7].[0-9]{4}',
-				sixMonthsAmount: '[1-7].[0-9]{4}',
-				oneYearAmount: '[1-7].[0-9]{4}',
-				threeYearsAmount: '[1-7].[0-9]{4}',
+				thirtyDaysAmount: '[2-7].[0-9]{4}',
+				sixMonthsAmount: '[2-7].[0-9]{4}',
+				oneYearAmount: '[2-7].[0-9]{4}',
+				threeYearsAmount: '[5-9].[0-9]{4}',
 			},
 			previewInfo: {
 				swap: {
