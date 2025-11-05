6262d8ec28040fd580b0ed0a2954e913094fa5cb
juan-langa <juan@oazoapps.com>
2025-11-05 10:46:04 +0100

    Daily improvements - November - 05

--- tests/withWallet/aaveV3/base/aaveV3EarnBase.spec.ts
+++ tests/withWallet/aaveV3/base/aaveV3EarnBase.spec.ts
@@ -15,7 +15,7 @@ let walletAddress: string;
 const test = testWithSynpress(metaMaskFixtures(baseSetup));
 
 // SKIP - Failing and getting stuck in Nightly run
-test.describe.skip('Aave V3 Earn - Base - Wallet connected', async () => {
+test.describe('Aave V3 Earn - Base - Wallet connected', async () => {
 	test.beforeEach(async ({ metamask, page }) => {
 		test.setTimeout(longTestTimeout);
 
