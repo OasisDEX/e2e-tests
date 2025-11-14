491e079bde17f2e4bc52ce60dee687f0da2d3154
juan-langa <juan@oazoapps.com>
2025-11-14 10:51:10 +0100

    Daily improvements - November - 14

--- testsEarnProtocol/noWallet/portfolioSumrRewards.spec.ts
+++ testsEarnProtocol/noWallet/portfolioSumrRewards.spec.ts
@@ -2,10 +2,25 @@ import { test } from '#earnProtocolFixtures';
 
 test.describe('Portfolio > SUMR Rewards tab', async () => {
 	test.beforeEach(async ({ app }) => {
-		await app.portfolio.open('0x10649c79428d718621821Cf6299e91920284743F');
+		await app.portfolio.open('0x10649c79428d718621821Cf6299e91920284743F?tab=rewards');
 	});
 
-	test('It should disply SUMR price', async ({ app }) => {
-		// await app.portfolio.rewards.
+	test('It should display SUMR price', async ({ app }) => {
+		await app.portfolio.rewards.shoulHaveSumrPrice({
+			price: '[0-9].[0-9]{2,4}',
+			marketCap: '([0-9]{1,3},)?([0-9]{1,3},)?([0-9]{1,3},)?[0-9]{1,3}',
+			valuation: '([0-9]{1,3},)?([0-9]{1,3},)?([0-9]{1,3},)?[0-9]{1,3}',
+			holders: '([0-9]{1,3},)?[0-9]{1,3}',
+			sevenDaysTrend: '[0-9]{1,3}.[0-9]{2}',
+			thirtyDaysTrend: '[0-9]{1,3}.[0-9]{2}',
+			ninetyDaysTrend: '[0-9]{1,3}.[0-9]{2}',
+		});
+	});
+
+	test('It should buy SUMR', async ({ app }) => {
+		await app.portfolio.rewards.buySumr();
+
+		// TO BE DONE
+		// -- Redirects to non-existent page for now
 	});
 });
