1ea7f44095672f6d35bbd9a2be55877c86130a6f
juan-langa <juan@oazoapps.com>
2025-12-04 17:32:13 +0100

    Daily improvements - December - 04

--- srcEarnProtocol/pages/staking/manage.ts
+++ srcEarnProtocol/pages/staking/manage.ts
@@ -15,4 +15,56 @@ export class Manage {
 			'"Stake SUMR to earn rewards" header shouldbe visible'
 		).toBeVisible();
 	}
+
+	@step
+	async openPage() {
+		await expect(async () => {
+			await this.page.goto('/earn/staking/manage');
+			await this.shouldBeVisible();
+		}).toPass();
+	}
+
+	@step
+	async sumrAmountToStake(amount: string) {
+		await this.page.locator('[class*="_inputWrapper_"] input').fill(amount);
+	}
+
+	@step
+	async acceptPenaltyWarning() {
+		await this.page.locator('[class*="_checkboxSectionWrapper_"]').click();
+	}
+
+	@step
+	async approveSumr() {
+		await this.page.getByRole('button', { name: 'Approve SUMR' }).click();
+	}
+
+	/**
+	 * @param days should be between '0' and '1080' both included | 0: far left | 1080: far right
+	 */
+	@step
+	async selectLockupDays(days: number) {
+		const value = days / 1080;
+
+		await expect(async () => {
+			const initialSliderValue = await this.page
+				.locator('input[type="range"]')
+				.getAttribute('value');
+
+			const slider = this.page.locator('input[type="range"]');
+			const sliderBoundingBox = (await slider.boundingBox()) ?? { x: 0, y: 0, width: 0, height: 0 };
+
+			await slider.dragTo(slider, {
+				force: true,
+				targetPosition: {
+					x: sliderBoundingBox.width * value,
+					y: 0,
+				},
+			});
+
+			const newSliderValue = await this.page.locator('input[type="range"]').getAttribute('value');
+
+			expect(newSliderValue !== initialSliderValue).toBe(true);
+		}).toPass();
+	}
 }
---
+++ testsEarnProtocol/withRealWallet/stakingManage.spec.ts
@@ -0,0 +1,37 @@
+import { testWithSynpress } from '@synthetixio/synpress';
+import { test as withRealWalletBaseFixtures } from '../../srcEarnProtocol/fixtures/withRealWalletBase';
+import { logInWithWalletAddress } from 'srcEarnProtocol/utils/logIn';
+import { expectDefaultTimeout } from 'utils/config';
+
+const test = testWithSynpress(withRealWalletBaseFixtures);
+
+test.describe('Staking > Manage page', async () => {
+	test.beforeEach(async ({ app, metamask }, testInfo) => {
+		testInfo.setTimeout(testInfo.timeout + 25_000);
+
+		await logInWithWalletAddress({
+			metamask,
+			app,
+			wallet: 'MetaMask',
+		});
+
+		await app.staking.manage.openPage();
+	});
+
+	test('It should stake SUMR - No lockup - Until tx approval', async ({ app, metamask }) => {
+		await app.staking.manage.sumrAmountToStake('10');
+		await app.staking.manage.acceptPenaltyWarning();
+		await app.staking.manage.approveSumr();
+
+		await metamask.rejectTransaction();
+	});
+
+	test('It should stake SUMR - <3m locup slot - Until tx approval', async ({ app, metamask }) => {
+		await app.staking.manage.sumrAmountToStake('10');
+		await app.staking.manage.selectLockupDays(60);
+
+		await app.staking.manage.acceptPenaltyWarning();
+		await app.staking.manage.approveSumr();
+		await metamask.rejectTransaction();
+	});
+});
