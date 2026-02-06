5d8e49c445e4b768b4a39a4b3505b53d0178699f
juan-langa <juan@oazoapps.com>
2026-02-06 14:07:37 +0100

    Daily improvements - february - 06

--- srcInstitutions/pages/clientDashboard/vaults/vaultExposure.ts
+++ srcInstitutions/pages/clientDashboard/vaults/vaultExposure.ts
@@ -13,7 +13,7 @@ export class VaultExposure {
 	async shouldBeVisible(args?: { timeout: number }) {
 		await expect(
 			this.page.getByRole('heading', { name: 'Vault exposure' }),
-			'"Vault exposure" header should be visible'
+			'"Vault exposure" header should be visible',
 		).toBeVisible({ timeout: args?.timeout ?? expectDefaultTimeout });
 	}
 
@@ -21,21 +21,21 @@ export class VaultExposure {
 	async shouldHaveAssetAllocationBar() {
 		await expect(
 			this.page.getByText('Asset allocation'),
-			'"Asset allocation" label should be visible'
+			'"Asset allocation" label should be visible',
 		).toBeVisible();
 
 		await expect(
 			this.page.locator('[class*="_allocationBarItem_"]').first(),
-			'Asset allocation bar should be visible'
+			'Asset allocation bar should be visible',
 		).toBeVisible();
 	}
 
 	@step
-	async shouldHaveVaultExposurePanel() {
+	async shouldHaveVaultExposurePanel(args?: { timeout: number }) {
 		await expect(
 			this.page.locator('[class*="PanelVaultExposure_tableSection_"] tbody > tr > td').first(),
-			'First row of exposure table should be visible'
-		).toBeVisible();
+			'First row of exposure table should be visible',
+		).toBeVisible({ timeout: args?.timeout ?? expectDefaultTimeout });
 	}
 
 	@step
@@ -43,7 +43,7 @@ export class VaultExposure {
 		const allocations = (
 			await this.page
 				.locator(
-					'[class*="PanelVaultExposure_tableSection_"] tr > td:nth-child(1) p:has-text("allocated")'
+					'[class*="PanelVaultExposure_tableSection_"] tr > td:nth-child(1) p:has-text("allocated")',
 				)
 				.allInnerTexts()
 		).map((text) => parseFloat(text.replace('New!', '').replace('% allocated', '')));
@@ -87,5 +87,42 @@ export class VaultExposure {
 		const apys = await this.getStrategiesApys();
 
 		expect(apys.includes('0.00%')).not.toBeTruthy();
+	}
+
+	@step
+	async shouldHaveArksOnChain(
+		arks: {
+			name: string;
+			apy?: string;
+			protocolTvl?: string;
+			description?: string;
+		}[],
+	) {
+		for (const ark of arks) {
+			const arkLocator = this.page
+				.locator('[class*="_availableArksSection_"] [class*="_cardSecondary_"]')
+				.filter({ hasText: ark.name });
+
+			await expect(arkLocator, `${ark.name} ark should be listed`).toBeVisible();
+
+			if (ark.apy) {
+				const regExp = new RegExp(`${ark.apy}%`);
+				await expect(arkLocator.getByText('APY:'), `It should have ${regExp} APY`).toContainText(
+					regExp,
+				);
+			}
+
+			if (ark.protocolTvl) {
+				const regExp = new RegExp(ark.protocolTvl);
+				await expect(
+					arkLocator.getByText('Protocol TVL:'),
+					`It should have ${regExp} TVL`,
+				).toContainText(regExp);
+			}
+
+			if (ark.description) {
+				await expect(arkLocator.getByText(ark.description)).toBeVisible();
+			}
+		}
 	}
 }
--- testsInstitutions/noWallet/vaults/vaultExposure.spec.ts
+++ testsInstitutions/noWallet/vaults/vaultExposure.spec.ts
@@ -1,55 +1,57 @@
 import { expect, test } from '#institutionsNoWalletFixtures';
 import { signIn } from 'srcInstitutions/utils/signIn';
+import { expectDefaultTimeout } from 'utils/config';
 
-test.describe('Client dashboard - Vaults - Vault exposure', async () => {
-	test.beforeEach(async ({ app }) => {
-		await signIn({ app, userRights: 'client', role: 'Viewer' });
-		await app.clientDashboard.selectTab('Vaults');
-		await app.clientDashboard.vaults.selectPanel('Vault exposure');
-	});
+['USDC Base', 'USDC arbitrum'].forEach((vault) =>
+	test.describe('Client dashboard - Vaults - Vault exposure', async () => {
+		test.beforeEach(async ({ app }) => {
+			await signIn({ app, userRights: 'client', role: 'Viewer' });
+			await app.clientDashboard.selectTab('Vaults');
 
-	// TO DO
-	//  - For diferent vaults --> ([vault1, vault2]).forEach(test())
+			if (vault === 'USDC arbitrum') {
+				// Switch to Arbitrum USDC vault
+				await app.clientDashboard.vaults.openVaultsDropdown();
+				await app.clientDashboard.vaults.selectVault('ExtDemoCorp USDC arbitrum');
+				await app.clientDashboard.vaults.shouldHaveVaultHeader({
+					name: 'ExtDemoCorp USDC arbitrum',
+				});
+			}
 
-	test('It should show asset allocation - Default view', async ({ app }) => {
-		// TO DO - Select vault from vaults' dropdown
-		// await app.clientDashboard.vaults.openVaultsDropdown();
+			await app.clientDashboard.vaults.selectPanel('Vault exposure');
+			// Wait for Exposure panel to load
+			await app.clientDashboard.vaults.vaultExposure.shouldHaveVaultExposurePanel({
+				timeout: expectDefaultTimeout * 3,
+			});
+		});
 
-		await app.clientDashboard.vaults.vaultExposure.shouldHaveAssetAllocationBar();
-		await app.clientDashboard.vaults.vaultExposure.shouldHaveVaultExposurePanel();
-	});
+		test(`It should show asset allocation - ${vault}`, async ({ app }) => {
+			await app.clientDashboard.vaults.vaultExposure.shouldHaveAssetAllocationBar();
+		});
 
-	test('It should show strategies exposure and be 100% in total', async ({ app }) => {
-		// TO DO - Select vault from vaults' dropdown
-		// await app.clientDashboard.vaults.openVaultsDropdown();
+		test(`It should show strategies exposure and be 100% in total - ${vault}`, async ({ app }) => {
+			const totalAllocationBase =
+				await app.clientDashboard.vaults.vaultExposure.getStrategiesTotalAllocation();
 
-		// Wait for Exposure panel to load
-		await app.clientDashboard.vaults.vaultExposure.shouldHaveVaultExposurePanel();
+			expect(totalAllocationBase).toBeGreaterThan(99);
+			expect(totalAllocationBase).toBeLessThanOrEqual(100);
+		});
 
-		const totalAllocation =
-			await app.clientDashboard.vaults.vaultExposure.getStrategiesTotalAllocation();
+		test(`It should not have duplicated strategy names - ${vault}`, async ({ app }) => {
+			await app.clientDashboard.vaults.vaultExposure.shouldNotHaveDuplicatedStrategyNames();
+		});
 
-		expect(totalAllocation).toBeGreaterThan(99);
-		expect(totalAllocation).toBeLessThanOrEqual(100);
-	});
+		test(`It should not have 0.00% APY for any arks - ${vault}`, async ({ app }) => {
+			await app.clientDashboard.vaults.vaultExposure.shouldNotHaveStrategyApysEqualToZero();
+		});
 
-	test('It should not have duplicated strategy names', async ({ app }) => {
-		// TO DO - Select vault from vaults' dropdown
-		// await app.clientDashboard.vaults.openVaultsDropdown();
-
-		// Wait for Exposure panel to load
-		await app.clientDashboard.vaults.vaultExposure.shouldHaveVaultExposurePanel();
-
-		await app.clientDashboard.vaults.vaultExposure.shouldNotHaveDuplicatedStrategyNames();
-	});
-
-	test('It should not have 0.00% APY for any arks', async ({ app }) => {
-		// TO DO - Select vault from vaults' dropdown
-		// await app.clientDashboard.vaults.openVaultsDropdown();
-
-		// Wait for Exposure panel to load
-		await app.clientDashboard.vaults.vaultExposure.shouldHaveVaultExposurePanel();
-
-		await app.clientDashboard.vaults.vaultExposure.shouldNotHaveStrategyApysEqualToZero();
-	});
-});
+		test(`It should have arks available on chain- ${vault}`, async ({ app }) => {
+			await app.clientDashboard.vaults.vaultExposure.shouldHaveArksOnChain([
+				{
+					name: 'Morpho USDC Gauntlet Core',
+					apy: '[0-9]{1,2}.[0-9]{1,2}',
+					protocolTvl: '[0-9]{1,3}.[0-9]{1,2}[BM]',
+				},
+			]);
+		});
+	}),
+);
