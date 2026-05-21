15c8b5fafdb36b16a7a8eeb336e0f6eccd332ee9
juan-langa <juan@oazoapps.com>
2026-05-21 09:52:33 +0200

    Test updates & first DCA tests and framework

--- srcEarnProtocol/pages/dca/index.ts
+++ srcEarnProtocol/pages/dca/index.ts
@@ -1,11 +1,19 @@
 import { expect, step } from '#earnProtocolFixtures';
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 export class Dca {
 	readonly page: Page;
 
+	readonly startingVaultBlockLocator: Locator;
+
+	readonly targetVaultBlockLocator: Locator;
+
 	constructor(page: Page) {
 		this.page = page;
+		this.startingVaultBlockLocator = page
+			.locator('[class*="dca_vaultDropdownWrapperFrom_"]')
+			.first();
+		this.targetVaultBlockLocator = page.locator('[class*="dca_vaultDropdownWrapperTo_"]').first();
 	}
 
 	@step
@@ -32,4 +40,40 @@ export class Dca {
 			/selected/,
 		);
 	}
+
+	@step
+	async openStartingVaultDropdown() {
+		await this.startingVaultBlockLocator.click();
+	}
+
+	@step
+	async openTargetVaultDropdown() {
+		await this.targetVaultBlockLocator.click();
+	}
+
+	@step
+	async shouldHaveVaultDropdownOpened(vault: 'starting' | 'target') {
+		const dropdownLocator = {
+			starting: this.startingVaultBlockLocator,
+			target: this.targetVaultBlockLocator,
+		};
+
+		await expect(dropdownLocator[vault].locator('[class*="_dropdownOptions_"]')).toHaveClass(
+			/dropdownShow/,
+		);
+		await expect(dropdownLocator[vault].locator('[class*="_dropdownOptions_"]')).toBeVisible();
+	}
+
+	// @step
+	// async selectStartingVault({
+	// 	riskManagementType,
+	// 	token,
+	// 	riskLevel,
+	// }: {
+	// 	riskManagementType: RiskManagementTypes;
+	// 	token: 'ETH' | 'USDC' | 'USDT';
+	// 	riskLevel?: RiskLevels;
+	// }) {
+	// 	await this.page.locator('[class*="dca_vaultDropdownWrapperFrom_"]').first().click();
+	// }
 }
